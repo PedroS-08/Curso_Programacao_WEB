5e80771f736953c36b4952a9c92b4926a9725726
Pedro Silveira <pedro.esn08@gmail.com>
2026-05-18 18:39:56 -0300

    hj

--- MODELO_Pagina/script.js
+++ MODELO_Pagina/script.js
@@ -3,6 +3,7 @@ function aba(secao) {
     document.getElementById(secao).style.display = "block";
 }
 
+let produtos = []
 
 function cadastrar() {
     let nome_prod = document.getElementById("nome_prod");
@@ -11,10 +12,9 @@ function cadastrar() {
     let preco_venda = document.getElementById("preco_venda");
     let preco_custo = document.getElementById("preco_custo");
     let qtd = document.getElementById("qtd");
-
     let rad = document.querySelector('input[name="atv"]:checked');
-    let radValue = rad ? rad.value.trim() : '';
 
+    let radValue = rad ? rad.value.trim() : '';
     let name = nome_prod.value.trim();
     let txt = txt_area.value.trim();
     let slc = selec.value.trim();
@@ -27,11 +27,60 @@ function cadastrar() {
         return;
     }
 
-    let listaProd = document.getElementById("lista_prod");
-    let novoProd = document.createElement("li");
-    novoProd.textContent = `${name} | ${txt} | ${slc} | ${radValue} | ${pv} | ${pc} | ${qt}`;
-    listaProd.appendChild(novoProd);
+    let produto = {
+        nome: name,
+        descricao: txt,
+        categoria: slc,
+        status: radValue,
+        venda: pv,
+        custo: pc,
+        quantidade: qt
+    };
 
-    document.getElementById("mensagem").textContent = "Produto registrado!";
-    
+    produtos.push(produto);
+    mostrarProdutos(produtos);
+    document.getElementById("mensagem").textContent =
+    "Produto registrado!";
+}
+
+function mostrarProdutos(lista) {
+    let listaProd = document.getElementById("lista_prod");
+    listaProd.innerHTML = "";
+
+    lista.forEach(prod => {
+        listaProd.innerHTML += `
+            <tr>
+                <td>${prod.nome}</td>
+                <td>${prod.categoria}</td>
+                <td>${prod.venda}</td>
+                <td>${prod.quantidade}</td>
+            </tr>
+        `;
+    });
+}
+
+
+function filtrar(categoria) {
+
+    if (categoria == "Todos") {
+        mostrarProdutos(produtos);
+
+    } else {
+
+        let filtrados = produtos.filter(prod =>
+            prod.categoria == categoria
+        );
+        mostrarProdutos(filtrados);
+    }
+}
+
+function limpar() {
+    document.getElementById("nome_prod").value = "";
+    document.getElementById("txt_area").value = "";
+    document.getElementById("selec").selectedIndex = 0;
+    document.getElementById("preco_venda").value = "";
+    document.getElementById("preco_custo").value = "";
+    document.getElementById("qtd").value = "";
+    document.querySelectorAll('input[name="atv"]').forEach(radio => { radio.checked = false;});
+    document.getElementById("mensagem").textContent = "";
 }
