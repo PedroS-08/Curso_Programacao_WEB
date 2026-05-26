d625af360d96a6befce9feb6be6a093ccc9cf21f
Pedro Silveira <pedro.esn08@gmail.com>
2026-05-26 10:21:05 -0300

    atv

--- MODELO_Pagina/script.js
+++ MODELO_Pagina/script.js
@@ -3,10 +3,7 @@ function aba(secao) {
     document.getElementById(secao).style.display = "block";
 }
 
-<<<<<<< HEAD
 let produtos = []
-=======
->>>>>>> 4a22af985acb9daff8ccd4a3edb2411205e10f7e
 
 function cadastrar() {
     let nome_prod = document.getElementById("nome_prod");
@@ -15,16 +12,9 @@ function cadastrar() {
     let preco_venda = document.getElementById("preco_venda");
     let preco_custo = document.getElementById("preco_custo");
     let qtd = document.getElementById("qtd");
-<<<<<<< HEAD
     let rad = document.querySelector('input[name="atv"]:checked');
 
     let radValue = rad ? rad.value.trim() : '';
-=======
-
-    let rad = document.querySelector('input[name="atv"]:checked');
-    let radValue = rad ? rad.value.trim() : '';
-
->>>>>>> 4a22af985acb9daff8ccd4a3edb2411205e10f7e
     let name = nome_prod.value.trim();
     let txt = txt_area.value.trim();
     let slc = selec.value.trim();
@@ -37,7 +27,6 @@ function cadastrar() {
         return;
     }
 
-<<<<<<< HEAD
     let produto = {
         nome: name,
         descricao: txt,
@@ -94,13 +83,4 @@ function limpar() {
     document.getElementById("qtd").value = "";
     document.querySelectorAll('input[name="atv"]').forEach(radio => { radio.checked = false;});
     document.getElementById("mensagem").textContent = "";
-=======
-    let listaProd = document.getElementById("lista_prod");
-    let novoProd = document.createElement("li");
-    novoProd.textContent = `${name} | ${txt} | ${slc} | ${radValue} | ${pv} | ${pc} | ${qt}`;
-    listaProd.appendChild(novoProd);
-
-    document.getElementById("mensagem").textContent = "Produto registrado!";
-    
->>>>>>> 4a22af985acb9daff8ccd4a3edb2411205e10f7e
 }
